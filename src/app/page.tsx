be8327693239fd6f3c736b52e7146c00a834eddb
Artem <zezebolz@gmail.com>
2023-12-09 14:49:13 +0300

    refactored some code

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,10 +1,10 @@
-import { LoginForm } from "@/components/LoginForm";
+import AuthForm from "@/components/AuthForm";
 import Image from "next/image";
 
 export default function Home() {
 	return (
 		<main className="flex min-h-screen flex-col items-center justify-center">
-			<LoginForm />
+			<AuthForm />
 		</main>
 	);
 }
--- src/components/AuthForm.tsx
+++ src/components/AuthForm.tsx
@@ -2,12 +2,15 @@
 
 import { SignInResponse, signIn, useSession } from "next-auth/react";
 import { useRouter } from "next/navigation";
-import { useEffect } from "react";
+import { useEffect, useRef } from "react";
 import toast from "react-hot-toast";
 
-export const LoginForm = () => {
+const AuthForm = () => {
 	const session = useSession();
 	const router = useRouter();
+
+	const usernameRef = useRef<HTMLInputElement>(null);
+	const passwordRef = useRef<HTMLInputElement>(null);
 
 	useEffect(() => {
 		if (session.status === "authenticated") {
@@ -28,48 +31,50 @@ export const LoginForm = () => {
 
 	const handleSubmit = (e: React.FormEvent<HTMLFormElement>) => {
 		e.preventDefault();
-		const login = e.currentTarget.login.value;
-		const password = e.currentTarget.password.value;
+
+		const credentials = {
+			username: usernameRef.current?.value,
+			password: passwordRef.current?.value,
+		};
 
 		signIn("credentials", {
-			username: login,
-			password: password,
+			...credentials,
 			redirect: false,
 		})
 			.then((callback) => {
 				checkCredentials(callback!);
 			})
-			.catch((err) => {
-				console.log(err);
+			.catch(() => {
+				toast.error("Something went wrong");
 			});
 	};
 
 	return (
-		<div className="w-[350px] p-[25px] box-content bg-white rounded-[10px] flex flex-col">
+		<div className="w-[350px] p-[25px] box-content bg-white rounded-md flex flex-col">
 			<form onSubmit={handleSubmit}>
 				<h2 className="text-[20px]">Welcome to admin panel</h2>
-				<div className="mt-[20px]">
-					<div className="w-full">
-						<label htmlFor="login">Login</label>
-						<input
-							id="login"
-							className="bg-slate-300 w-full mt-[10px] h-[40px] p-[10px] rounded-[10px] focus:border-red-500"
-							type="text"
-						/>
-					</div>
+				<div className="mt-[20px] w-full">
+					<label htmlFor="username">Username</label>
+					<input
+						ref={usernameRef}
+						id="username"
+						className="bg-slate-300 w-full mt-[10px] h-[40px] p-[10px] rounded-md focus:border-red-500"
+						type="text"
+					/>
 				</div>
 				<div className="mt-[20px] w-full">
 					<label htmlFor="password">Password</label>
 					<input
+						ref={passwordRef}
 						id="password"
-						className="bg-slate-300 w-full h-[40px] mt-[10px] p-[10px] rounded-[10px]"
+						className="bg-slate-300 w-full h-[40px] mt-[10px] p-[10px] rounded-md"
 						type="password"
 					/>
 				</div>
 				<div className="mt-[20px] w-full">
 					<button
 						type="submit"
-						className="py-[8px] w-full bg-black text-white text-[16px] rounded-[10px] hover:bg-slate-800 transition duration-300"
+						className="py-[8px] w-full bg-black text-white text-[16px] rounded-md hover:bg-slate-800 transition duration-300"
 					>
 						Login
 					</button>
@@ -78,3 +83,5 @@ export const LoginForm = () => {
 		</div>
 	);
 };
+
+export default AuthForm;
