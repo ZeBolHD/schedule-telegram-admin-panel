71d1d83e46ddc0a34e857d675d56408cbd55887a
ZeBolHD <zezebolz@gmail.com>
2023-12-10 20:09:36 +0300

    added groups api route

--- src/app/api/groups/route.ts
+++ src/app/api/groups/route.ts
@@ -1,1 +1,16 @@
-export async function GET() {}
+import { NextResponse } from "next/server";
+import { authOptions } from "../auth/[...nextauth]/route";
+import { getServerSession } from "next-auth";
+import prisma from "@/libs/prismadb";
+
+export async function GET() {
+	const session = await getServerSession(authOptions);
+
+	if (!session || !session.user) {
+		return NextResponse.json({ error: "Unauthorized" }, { status: 401 });
+	}
+
+	const groups = await prisma.group.findMany({});
+
+	return NextResponse.json(groups);
+}
